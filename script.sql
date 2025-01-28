f0ab0fa0d340daf0755203df32088c105d7024ec
richxrd12 <richardquintanapadron@gmail.com>
2025-01-28 15:10:57 +0000

    Comentarios extras y alguna pequeña modificación

--- script.sql
+++ script.sql
@@ -25,24 +25,24 @@ CREATE TABLE sesiones (
 );
 
 INSERT INTO usuarios (nombre, estado, usuario, password) VALUES
-('Richard', 'Im not God, but I have her on her knees', 'richard', '1234'),
-('Betsaida', '¿No? ¿No?', 'betsaida', '1234'),
-('Luca', 'Teta', 'luca', '1234'),
-('Víctor', 'Me gusta el zumito', 'victor', '1234'),
-('Saúl', ':D', 'saul', '1234'),
-('Álvaro', 'Autobús', 'alvaro', '1234'),
-('Óscar', 'Cogito ergo sum', 'oscar', '1234'),
-('Javier', 'Disturbing the peaceeeee', 'javier', '1234'),
-('Eliazar', 'Perdí la guagua profe', 'eliazar', '1234'),
-('Lin', 'Me estoy sacando el B1 profe', 'lin', '1234'),
-('Jazael', ':|', 'jazael', '1234'),
-('Daniel', 'De autobuses no sé, de guaguas sí hay huelga', 'daniel', '1234'),
-('Antonio', 'Antooooonio', 'antonio', '1234'),
-('Enrique', ':|', 'enrique', '1234'),
-('Ángel', 'Que fue jefa', 'angel', '1234'),
-('Flavio', 'En verdad me llamo Fabio', 'flavio', '1234'),
-('Edu', 'Delegado el que tengo aquí colgado', 'edu', '1234'),
-('Luis', 'Van a suspender todos!!', 'luis', '1234');
+('Richard', 'Im not God, but I have her on her knees', 'richard', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Betsaida', '¿No? ¿No?', 'betsaida', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Luca', 'Teta', 'luca', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Víctor', 'Me gusta el zumito', 'victor', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Saúl', ':D', 'saul', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Álvaro', 'Autobús', 'alvaro', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Óscar', 'Cogito ergo sum', 'oscar', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Javier', 'Disturbing the peaceeeee', 'javier', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Eliazar', 'Perdí la guagua profe', 'eliazar', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Lin', 'Me estoy sacando el B1 profe', 'lin', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Jazael', ':|', 'jazael', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Daniel', 'De autobuses no sé, de guaguas sí hay huelga', 'daniel', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Antonio', 'Antooooonio', 'antonio', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Enrique', ':|', 'enrique', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Ángel', 'Que fue jefa', 'angel', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Flavio', 'En verdad me llamo Fabio', 'flavio', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Edu', 'Delegado el que tengo aquí colgado', 'edu', '81dc9bdb52d04dc20036dbd8313ed055'),
+('Luis', 'Van a suspender todos!!', 'luis', '81dc9bdb52d04dc20036dbd8313ed055');
 
 INSERT INTO mensajes (mensaje, idDestinatario, idRemitente, fecha) VALUES
 ('Hola, ¿cómo estás?', 2, 1, '2025-01-14 10:00:00'),
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import com.google.gson.GsonBuilder;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.MessageDigest;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,7 +30,7 @@ public class Main {
 
             Socket clienteEscucha = serverEscucha.accept();
 
-            //Le mandamos un código de éxito para que no se quede bloqueado el hilo del otro servidor
+            //Le mandamos un código de éxito para que no se quede bloqueado el hilo por culpa del otro Socket
             ObjectOutputStream outputStreamEscucha = new ObjectOutputStream(clienteEscucha.getOutputStream());
             outputStreamEscucha.writeObject("Conexión establecida");
             outputStreamEscucha.flush();
@@ -43,6 +44,10 @@ public class Main {
 
     public static void manejarCliente(Socket cliente, Socket clienteEscucha, Map<String, ObjectOutputStream> conectados,
                                       ObjectOutputStream salida, ObjectOutputStream salidaEscucha) {
+
+        int idCliente = 0;
+        int idContacto = 0;
+
         try {
 
             // Crea los flujos de entrada y salida una sola vez por cliente
@@ -51,9 +56,6 @@ public class Main {
             //Crea los flujos de entrada y salida de la escucha una vez por cliente
             ObjectInputStream entradaEscucha = new ObjectInputStream(clienteEscucha.getInputStream());
 
-            int idCliente = 0;
-            int idContacto = 0;
-
             while (true) {
 
                 // Escuchar y procesar solicitudes
@@ -61,6 +63,11 @@ public class Main {
                 String peticion = datos.get("peticion");
 
                 switch (peticion) {
+                    /**
+                     * Si la petición es login, borra el posible id del antiguo login,
+                     * nos comprueba si el id y la contraseña son correctas y nos devuelve el id del
+                     * cliente directamente.
+                     */
                     case "login":
                         conectados.remove(String.valueOf(idCliente));
 
@@ -72,10 +79,13 @@ public class Main {
                         salida.flush();
 
                         break;
-
+                    /**
+                     * Si la petición es register nos comprueba si el usuario está en uso, si no está en uso,
+                     * lo registra y nos manda el código "true", si no, nos manda el código de error "false"
+                      */
                     case "register":
-                        boolean correoUsed = comprobarUsuario(datos.get("correo"));
-                        if (!correoUsed) {
+                        boolean usuarioUsed = comprobarUsuario(datos.get("usuario"));
+                        if (!usuarioUsed) {
                             boolean registeredUser = registrarUsuario(datos.get("nombre"), datos.get("estado"),
                                     datos.get("usuario"), datos.get("password"));
                             salida.writeObject(registeredUser ? "true" : "false");
@@ -87,6 +97,10 @@ public class Main {
 
                         break;
 
+                    /**
+                     * Si la petición es contactos, este nos crea un objeto de ListaUsuarios con una lista de usuarios
+                     * para serializar en JSON, lo serializa y lo manda al cliente.
+                      */
                     case "contactos":
                         // Procesa los contactos
                         ListaUsuarios listaUsuarios = new ListaUsuarios(obtenerContactos(conectados));
@@ -99,10 +113,13 @@ public class Main {
 
                         break;
 
+                    /**
+                     * Si nos pide los mensajes, tenemos que pedir los mensajes del contacto al que está entrando en el
+                     * chat, los pide, los serializa a JSON y los manda.
+                      */
                     case "peticion-mensajes":
                         idContacto = Integer.parseInt(datos.get("idContacto"));
 
-                        System.out.println(idCliente + "cliente, contacto: " + idContacto);
                         ArrayList<Mensaje> mensajes = obtenerMensajes(idCliente, idContacto);
 
                         Gson gsonMensajes = new GsonBuilder().setPrettyPrinting().create();
@@ -114,6 +131,12 @@ public class Main {
 
                         break;
 
+                    /**
+                     * Si la petición es envío mensaje, obtiene el mensaje, la fecha y con el idCliente (guardado al
+                     * hacer el login) y el idContacto (guardado al entrar en la conversación, es decir, cuando hacen la
+                     * petición de mensajes), guarda el mensaje en la base de datos y si el usuario está conectado,
+                     * le manda un código de "mensaje-recibido" de escucha para que se actualice la interfaz.
+                     */
                     case "envio-mensaje":
 
                         String mensajeEnviado = datos.get("mensaje");
@@ -136,7 +159,11 @@ public class Main {
                         break;
                 }
             }
+            /**
+             * Si el cliente sale de la app o da error, se desconecta (se borra de la lista de conectados)
+              */
         } catch (Exception e) {
+            conectados.remove(String.valueOf(idCliente));
             System.out.println(e);
         }
     }
@@ -158,7 +185,7 @@ public class Main {
             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY);
 
             preparedStatement.setString(1, usuario);
-            preparedStatement.setString(2, password);
+            preparedStatement.setString(2, encriptarMd5(password));
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -226,7 +253,7 @@ public class Main {
             preparedStatement.setString(1, nombre);
             preparedStatement.setString(2, estado);
             preparedStatement.setString(3, usuario);
-            preparedStatement.setString(4, password);
+            preparedStatement.setString(4, encriptarMd5(password));
 
             int rowsInserted = preparedStatement.executeUpdate();
 
@@ -285,7 +312,7 @@ public class Main {
         String user = "root";
         String pass = "1234";
         String driver = "com.mysql.cj.jdbc.Driver";
-        boolean isOnline = false;
+        boolean isOnline;
         Connection connection;
 
         final String SELECT_QUERY = "SELECT id, nombre, estado, usuario, password FROM usuarios";
@@ -328,7 +355,7 @@ public class Main {
 
         final String SELECT_QUERY = "SELECT id, mensaje, idDestinatario, idRemitente, fecha FROM mensajes " +
                 "WHERE idDestinatario = ? AND idRemitente = ? OR idDestinatario = ? AND idRemitente = ? " +
-                "ORDER BY fecha ASC;";
+                "ORDER BY id ASC;";
 
         try {
             Class.forName(driver);
@@ -350,6 +377,29 @@ public class Main {
             }
 
             return mensajes;
+
+        }catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public static String encriptarMd5(String password){
+        String output = "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try{
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+
+            byte[] digest = md5.digest(password.getBytes());
+
+            for (byte bit : digest){
+                stringBuilder.append(String.format("%02x", bit));
+            }
+
+            output = stringBuilder.toString();
+
+            return output;
 
         }catch (Exception e){
             System.out.println(e);
